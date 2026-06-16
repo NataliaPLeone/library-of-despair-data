2bdd5e4fc792677be6f0b9649857b2e9986b38dd
NataliaPLeone <119025431+NataliaPLeone@users.noreply.github.com>
2026-06-15 23:39:39 -0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,8 +1,13 @@
-CREATE TABLE incorrect_translation_suggestions (
+CREATE TABLE incorrect_transcription_suggestions (
     id SERIAL PRIMARY KEY,
     episode_line TEXT NOT NULL,
     correction_suggestion TEXT NOT NULL,
-    status TEXT DEFAULT 'pending'::TEXT
+    status TEXT DEFAULT 'pending'::TEXT,
+    episode_id INTEGER NOT NULL,
+    line_number INTEGER NOT NULL,
+    suggestion TEXT NOT NULL
+
 );
 
 
+    
